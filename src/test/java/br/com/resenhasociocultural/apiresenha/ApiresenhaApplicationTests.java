1739b4c2c670b55af53aadc36474784e804b3068
Arthur Góes <paschoal.ag@gmail.com>
2025-05-04 22:58:28 -0300

    chore: Initial project structure from Spring Initializr

---
+++ src/test/java/br/com/resenhasociocultural/apiresenha/ApiresenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.resenhasociocultural.apiresenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiresenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
