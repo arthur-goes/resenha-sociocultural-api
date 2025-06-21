4d0bfe426bef483b4d6c54bea46ac539c35baf95
Arthur Góes <paschoal.ag@gmail.com>
2025-06-20 23:56:06 -0300

    Add JPA validation to Meeting entity and include minutosDeSabedoriaLesson parameter

--- src/main/java/br/com/resenhasociocultural/apiresenha/model/Meeting.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/model/Meeting.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -11,20 +12,28 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor()
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Meeting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private LocalDate date;
 
+    @Column(nullable = false)
     private String theme;
+
+    @Column(nullable = false)
+    private String minutosDeSabedoriaLesson;
 
     @OneToMany(mappedBy = "meeting", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Attendance> presenceList;
 
     @OneToMany(mappedBy = "meeting", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Strike> strikes;
+
+    @OneToMany(mappedBy = "meeting", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ParticipationPoint> participationPoints;
 }
