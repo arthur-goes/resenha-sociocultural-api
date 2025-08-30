c154b3263b2c3156b1c7cc9e2fa1add31d78cd39
Arthur Góes <paschoal.ag@gmail.com>
2025-08-30 10:54:55 -0300

    feature(AttendanceFilterDto): Add validation to AttendanceFilterDto

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceController.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceController.java
@@ -2,11 +2,9 @@ package br.com.resenhasociocultural.apiresenha.features.attendance;
 
 import br.com.resenhasociocultural.apiresenha.features.attendance.dto.AttendanceFilterDto;
 import br.com.resenhasociocultural.apiresenha.features.attendance.dto.AttendanceResponseDto;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -18,14 +16,13 @@ public class AttendanceController {
     private final AttendanceService attendanceService;
     private final AttendanceMapper attendanceMapper;
 
-    AttendanceController(AttendanceService attendanceService, AttendanceMapper attendanceMapper){
+    AttendanceController(AttendanceService attendanceService, AttendanceMapper attendanceMapper) {
         this.attendanceService = attendanceService;
         this.attendanceMapper = attendanceMapper;
     }
 
     @GetMapping
-    public ResponseEntity<Set<AttendanceResponseDto>> find(@RequestParam AttendanceFilterDto dto)
-    {
+    public ResponseEntity<Set<AttendanceResponseDto>> find(@Valid @ModelAttribute AttendanceFilterDto dto) {
         Set<Attendance> attendances = attendanceService.findByFilter(dto);
         Set<AttendanceResponseDto> response = attendanceMapper.toResponseListDto(attendances);
         return ResponseEntity.ok(response);
--- src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/dto/AttendanceFilterDto.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/dto/AttendanceFilterDto.java
@@ -1,11 +1,19 @@
 package br.com.resenhasociocultural.apiresenha.features.attendance.dto;
 
+import jakarta.validation.constraints.PastOrPresent;
+
 import java.time.LocalDate;
 
 public record AttendanceFilterDto(
     String youthName,
+
+    @PastOrPresent
     LocalDate date,
+
+    @PastOrPresent
     LocalDate initialDate,
+
+    @PastOrPresent
     LocalDate finalDate
 ) {
 }
