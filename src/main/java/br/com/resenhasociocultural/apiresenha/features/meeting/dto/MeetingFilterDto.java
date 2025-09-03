7e9d7ab238f28115462cbc503fd449df8f3f4e46
Arthur Góes <paschoal.ag@gmail.com>
2025-09-03 12:37:31 -0300

    feat(validation): add Bean Validation to MeetingFilterDto

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/meeting/dto/MeetingFilterDto.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/meeting/dto/MeetingFilterDto.java
@@ -1,12 +1,24 @@
 package br.com.resenhasociocultural.apiresenha.features.meeting.dto;
 
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
 public record MeetingFilterDto(
+    @Pattern(
+        regexp = "^[A-Za-z]([A-Za-z\\s]*[A-Za-z])$",
+        message = "O tema pesquisado deve conter somente letras ou espaços, e sempre deve começar e terminar com uma letra"
+    )
     String theme,
+
+    @PastOrPresent
     LocalDate initialDate,
+
+    @PastOrPresent
     LocalDate finalDate,
+
+    @PastOrPresent
     LocalDate date
 ) {
 }
