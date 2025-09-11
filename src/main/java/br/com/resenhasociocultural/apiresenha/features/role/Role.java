4512aaf1a8172158d81eeaf7657235a1ed32aa80
Arthur Góes <paschoal.ag@gmail.com>
2025-09-11 16:42:14 -0300

    feat(security): add Security related entities UserProfile and Role

---
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/role/Role.java
@@ -0,0 +1,31 @@
+package br.com.resenhasociocultural.apiresenha.features.role;
+
+import br.com.resenhasociocultural.apiresenha.features.userprofile.UserProfile;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@Entity
+public class Role {
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<UserProfile> users;
+
+    public String getName(){
+        return "ROLE_" + this.name;
+    }
+}
---
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/userprofile/UserProfile.java
@@ -0,0 +1,48 @@
+package br.com.resenhasociocultural.apiresenha.features.userprofile;
+
+import br.com.resenhasociocultural.apiresenha.features.role.Role;
+import br.com.resenhasociocultural.apiresenha.shared.domain.Person;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "{id}")
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class UserProfile implements Person {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false, name = "creation_date")
+    @CreatedDate
+    private LocalDate creationDate;
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
