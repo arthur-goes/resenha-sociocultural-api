37080e6826968cedb7ae1b241aa1c702d7ad0edc
Arthur Góes <paschoal.ag@gmail.com>
2025-10-04 11:11:21 -0300

    add Role entity Data Transfer Objects

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/role/dto/RoleCreateDto.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/role/dto/RoleCreateDto.java
@@ -1,6 +1,9 @@
-package br.com.resenhasociocultural.apiresenha.features.role;
+package br.com.resenhasociocultural.apiresenha.features.role.dto;
+
+import jakarta.validation.constraints.NotBlank;
 
 public record RoleCreateDto(
+    @NotBlank
     String name
 ) {
 }
--- src/main/java/br/com/resenhasociocultural/apiresenha/features/role/dto/RoleResponseDto.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/role/dto/RoleResponseDto.java
@@ -1,4 +1,7 @@
 package br.com.resenhasociocultural.apiresenha.features.role.dto;
 
-public record RoleResponseDto() {
+public record RoleResponseDto(
+    Long id,
+    String name
+) {
 }
