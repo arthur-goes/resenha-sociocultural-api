5884b87963edb2e294bd905ad0fb3e07bb4c86d9
Arthur Góes <paschoal.ag@gmail.com>
2025-10-28 00:25:16 -0300

    test(builders): add attendance related DTOs builders
    This commit adds the following builders:
    
    - AttendanceCreateDtoBuilder
    - AttendanceFilterDtoBuilder

--- src/test/java/br/com/resenhasociocultural/apiresenha/features/attendance/builder/AttendanceCreateDtoBuilder.java
+++ src/test/java/br/com/resenhasociocultural/apiresenha/features/attendance/builder/AttendanceCreateDtoBuilder.java
@@ -1,4 +1,54 @@
 package br.com.resenhasociocultural.apiresenha.features.attendance.builder;
 
+import br.com.resenhasociocultural.apiresenha.features.attendance.AttendanceStatus;
+import br.com.resenhasociocultural.apiresenha.features.attendance.dto.AttendanceCreateDto;
+
 public class AttendanceCreateDtoBuilder {
+    private Long meetingId = 1L;
+    private Long youthId = 1L;
+    private AttendanceStatus status = AttendanceStatus.PRESENT;
+    private String absenceExcuse = null;
+
+    public static AttendanceCreateDtoBuilder anAttendanceCreateDto(){
+        return new AttendanceCreateDtoBuilder();
+    }
+
+    public AttendanceCreateDtoBuilder withMeetingId(Long id){
+        this.meetingId = id;
+        return this;
+    }
+
+    public AttendanceCreateDtoBuilder withYouthId(Long id){
+        this.youthId = id;
+        return this;
+    }
+
+    public AttendanceCreateDtoBuilder statusPresent(){
+        this.status = AttendanceStatus.PRESENT;
+        return this;
+    }
+
+    public AttendanceCreateDtoBuilder statusAbsent(){
+        this.status = AttendanceStatus.ABSENT;
+        return this;
+    }
+
+    public AttendanceCreateDtoBuilder statusExcusedAbsence(){
+        this.status = AttendanceStatus.EXCUSED_ABSENCE;
+        return this;
+    }
+
+    public AttendanceCreateDtoBuilder withAbsenceExcuse(){
+        this.absenceExcuse = absenceExcuse;
+        return this;
+    }
+
+    public AttendanceCreateDto build(){
+        return new AttendanceCreateDto(
+            meetingId,
+            youthId,
+            status,
+            absenceExcuse
+        );
+    }
 }
--- src/test/java/br/com/resenhasociocultural/apiresenha/features/attendance/builder/AttendanceFilterDtoBuilder.java
+++ src/test/java/br/com/resenhasociocultural/apiresenha/features/attendance/builder/AttendanceFilterDtoBuilder.java
@@ -1,4 +1,49 @@
-package br.com.resenhasociocultural.apiresenha.features.attendance.dto;
+package br.com.resenhasociocultural.apiresenha.features.attendance.builder;
 
-public class AttendanceFilterDtoBuilder {
+import br.com.resenhasociocultural.apiresenha.features.attendance.dto.AttendanceFilterDto;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public final class AttendanceFilterDtoBuilder {
+    private String youthName;
+    private @PastOrPresent LocalDate date;
+    private @PastOrPresent LocalDate initialDate;
+    private @PastOrPresent LocalDate finalDate;
+
+    private AttendanceFilterDtoBuilder() {
+    }
+
+    public static AttendanceFilterDtoBuilder anAttendanceFilterDto() {
+        return new AttendanceFilterDtoBuilder();
+    }
+
+    public AttendanceFilterDtoBuilder withYouthName(String youthName) {
+        this.youthName = youthName;
+        return this;
+    }
+
+    public AttendanceFilterDtoBuilder withDate(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    public AttendanceFilterDtoBuilder withInitialDate(LocalDate initialDate) {
+        this.initialDate = initialDate;
+        return this;
+    }
+
+    public AttendanceFilterDtoBuilder withFinalDate(LocalDate finalDate) {
+        this.finalDate = finalDate;
+        return this;
+    }
+
+    public AttendanceFilterDto build() {
+        return new AttendanceFilterDto(
+            youthName,
+            date,
+            initialDate,
+            finalDate
+        );
+    }
 }
