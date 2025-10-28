88641098dcadf8f067b790725d2e3e6d0e29f6d7
Arthur Góes <paschoal.ag@gmail.com>
2025-10-28 18:42:55 -0300

    refactor(mapper): remove unused method from AttendanceMapper

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceMapper.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceMapper.java
@@ -1,21 +1,17 @@
 package br.com.resenhasociocultural.apiresenha.features.attendance;
 
 import br.com.resenhasociocultural.apiresenha.features.attendance.dto.AttendanceCreateDto;
-import br.com.resenhasociocultural.apiresenha.features.attendance.dto.AttendanceForMeetingResponseDto;
 import br.com.resenhasociocultural.apiresenha.features.attendance.dto.AttendanceResponseDto;
 import br.com.resenhasociocultural.apiresenha.features.youth.YouthEntryMapper;
 import br.com.resenhasociocultural.apiresenha.features.youth.YouthMapper;
 import org.mapstruct.*;
 
-import java.util.List;
 import java.util.Set;
 
 @Mapper(componentModel = "spring", uses = {YouthMapper.class})
 public interface AttendanceMapper extends YouthEntryMapper<AttendanceEntry, AttendanceCreateDto> {
 
     Set<AttendanceResponseDto> toResponseListDto(Set<AttendanceEntry> attendance);
-    Set<AttendanceForMeetingResponseDto> toAttendanceForMeetingDto(List<AttendanceEntry> attendances);
-
     @Mapping(source = "meeting.id", target = "meetingId")
     @Mapping(source = "meeting.date", target = "date")
     AttendanceResponseDto toAttendanceResponse(AttendanceEntry attendance);
