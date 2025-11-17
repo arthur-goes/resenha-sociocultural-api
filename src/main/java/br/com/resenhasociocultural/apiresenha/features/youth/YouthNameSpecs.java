da45b062404cfab3d5fd4af562d3c917b39b9f8a
Arthur Góes <paschoal.ag@gmail.com>
2025-11-17 01:36:34 -0300

    feat(youth): add YouthNameSpecs for name/surname filtering
    This commit introduces the `YouthNameSpecs` class to provide reusable
    JPA Specifications for filtering entities by a youth's name.
    
    The class includes a generic method to create a case-insensitive "LIKE"
    predicate for searching a substring in both the `firstName` and `surname`
    fields. It offers two specific implementations:
    
    - `nameOrSurnameLikeForYouth`: For querying the `Youth` entity directly.
    - `nameOrSurnameLikeForYouthEntry`: For querying the `YouthEntry` entity
       via a join on `Youth`.

---
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/youth/YouthNameSpecs.java
@@ -0,0 +1,27 @@
+package br.com.resenhasociocultural.apiresenha.features.youth;
+
+import jakarta.persistence.criteria.*;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class YouthNameSpecs {
+    private <Entity> Specification<Entity> nameOrSurnameLike(String youthNameSubstring, Function<Root<Entity>,Path<Youth>> pathGenerator){
+        return (root, query, cb) -> {
+            Path<Youth> youthPath = pathGenerator.apply(root);
+            Predicate nameLike = cb.like(cb.upper(youthPath.get("firstName")), "%" + youthNameSubstring.toUpperCase() + "%");
+            Predicate surnameLike = cb.like(cb.upper(youthPath.get("firstName")), "%" + youthNameSubstring.toUpperCase() + "%");
+            return cb.or(nameLike, surnameLike);
+        };
+    }
+
+    public Specification<Youth> nameOrSurnameLikeForYouth(String nameSubstring){
+        return nameOrSurnameLike(nameSubstring, root -> root);
+    }
+
+    public Specification<YouthEntry> nameOrSurnameLikeForYouthEntry(String nameSubstring){
+        return nameOrSurnameLike(nameSubstring, root -> root.join("Youth"));
+    }
+}
