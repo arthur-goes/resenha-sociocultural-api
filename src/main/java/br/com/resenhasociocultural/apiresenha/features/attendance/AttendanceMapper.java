c39b6948310777665b040066c46f1c20f05f48b6
Arthur Góes <paschoal.ag@gmail.com>
2025-11-18 01:42:20 -0300

    refactor(attendance): Add list overload and rename method to DTO

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceMapper.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceMapper.java
@@ -6,14 +6,17 @@ import br.com.resenhasociocultural.apiresenha.features.youth.YouthEntryMapper;
 import br.com.resenhasociocultural.apiresenha.features.youth.YouthMapper;
 import org.mapstruct.*;
 
+import java.util.List;
 import java.util.Set;
 
 @Mapper(componentModel = "spring", uses = {YouthMapper.class})
 public interface AttendanceMapper extends YouthEntryMapper<AttendanceEntry, AttendanceCreateDto> {
 
-    Set<AttendanceResponseDto> toResponseListDto(Set<AttendanceEntry> attendance);
+    List<AttendanceResponseDto> toResponseListDto(Set<AttendanceEntry> attendance);
+    List<AttendanceResponseDto> toResponseListDto(List<AttendanceEntry> attendance);
+
     @Mapping(source = "meeting.id", target = "meetingId")
     @Mapping(source = "meeting.date", target = "date")
-    AttendanceResponseDto toAttendanceResponse(AttendanceEntry attendance);
+    AttendanceResponseDto toAttendanceResponseDto(AttendanceEntry attendance);
 
 }
