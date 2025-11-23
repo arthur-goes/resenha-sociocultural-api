dc9b8b3b1623ee9bab7d9be3ca5969d1c83a745f
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 12:11:47 -0300

    feat(youth) add validation to YouthSimpleDto

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/youth/dto/YouthSimpleDto.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/youth/dto/YouthSimpleDto.java
@@ -1,7 +1,15 @@
 package br.com.resenhasociocultural.apiresenha.features.youth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record YouthSimpleDto(
-        Long id,
-        String firstName,
-        String surname
+    @NotNull
+    Long id,
+
+    @NotBlank
+    String firstName,
+
+    @NotBlank
+    String surname
 ) implements YouthView {}
