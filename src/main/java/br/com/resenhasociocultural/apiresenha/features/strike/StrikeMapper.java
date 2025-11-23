8c64180b343323282b3202d33b1fa6e14ab79f38
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 11:29:54 -0300

    feat(strike): add response list mapping methods

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/strike/StrikeMapper.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/strike/StrikeMapper.java
@@ -5,10 +5,17 @@ import br.com.resenhasociocultural.apiresenha.features.strike.dto.StrikeCreateDt
 import br.com.resenhasociocultural.apiresenha.features.strike.dto.StrikeResponseDto;
 import br.com.resenhasociocultural.apiresenha.features.youth.YouthMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 import java.util.Set;
 
 @Mapper(componentModel = "spring", uses = {YouthMapper.class})
 public interface StrikeMapper extends YouthEntryMapper<StrikeEntry, StrikeCreateDto> {
-    Set<StrikeResponseDto> toResponseDto(Set<StrikeEntry> strikes);
+    @Mapping(source = "meeting.id", target = "meetingId")
+    StrikeResponseDto toResponseDto(StrikeEntry strike);
+
+    Set<StrikeResponseDto> toResponseDtoList(Set<StrikeEntry> strikes);
+    List<StrikeResponseDto> toResponseDtoList(List<StrikeEntry> strikes);
 
 }
