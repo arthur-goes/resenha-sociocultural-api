acec042df2d14f9114d9a10f09b929a5bbfdea75
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 10:34:48 -0300

    feat(attendance): add findById query method with youth join to repository

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceRepository.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/attendance/AttendanceRepository.java
@@ -2,5 +2,12 @@ package br.com.resenhasociocultural.apiresenha.features.attendance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AttendanceRepository extends JpaRepository<AttendanceEntry, Long>, JpaSpecificationExecutor<AttendanceEntry> {}
+import java.util.Optional;
+
+public interface AttendanceRepository extends JpaRepository<AttendanceEntry, Long>, JpaSpecificationExecutor<AttendanceEntry> {
+
+    @Query("SELECT a FROM AttendanceEntry a LEFT JOIN FETCH a.youth WHERE a.id = :id")
+    Optional<AttendanceEntry> findById();
+}
