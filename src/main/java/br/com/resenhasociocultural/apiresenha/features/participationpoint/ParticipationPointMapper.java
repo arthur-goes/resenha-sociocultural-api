589e227ccd4701950ab03db5f0572f27abdb572b
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 11:21:45 -0300

    feat(participationpoint): enhance mapper with response list mapping

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/participationpoint/ParticipationPointMapper.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/participationpoint/ParticipationPointMapper.java
@@ -5,8 +5,16 @@ import br.com.resenhasociocultural.apiresenha.features.participationpoint.dto.Pa
 import br.com.resenhasociocultural.apiresenha.features.participationpoint.dto.ParticipationPointResponseDto;
 import br.com.resenhasociocultural.apiresenha.features.youth.YouthMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+import java.util.Set;
 
 @Mapper(componentModel = "spring", uses = {YouthMapper.class})
 public interface ParticipationPointMapper extends YouthEntryMapper<ParticipationPointEntry, ParticipationPointCreateDto> {
+    @Mapping(source = "meeting.id", target = "meetingId")
     ParticipationPointResponseDto toResponseDto(ParticipationPointEntry entity);
+
+    Set<ParticipationPointResponseDto> toResponseDtoList(Set<ParticipationPointEntry> participationPoints);
+    List<ParticipationPointResponseDto> toResponseDtoList(List<ParticipationPointEntry> participationPoints);
 }
