15b8f63a714847356fb199535ac0514010d255bf
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 11:27:22 -0300

    feat(participationpoint): implement findById with a custom @Query to join youth

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/participationpoint/ParticipationPointRepository.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/participationpoint/ParticipationPointRepository.java
@@ -1,6 +1,12 @@
 package br.com.resenhasociocultural.apiresenha.features.participationpoint;
 
+import br.com.resenhasociocultural.apiresenha.features.attendance.AttendanceEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ParticipationPointRepository extends JpaRepository<ParticipationPointEntry, Long> {
+    @Query("SELECT p FROM ParticipationPointEntry p LEFT JOIN FETCH p.youth WHERE p.id = :id")
+    Optional<ParticipationPointEntry> findById();
 }
