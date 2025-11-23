835b423696ce08c8b980f21ecd0900e9e6ba923f
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 11:11:21 -0300

    feat(meeting): enhance mapper with creation DTO and response mapping
    - Add a `toCreateDto` method to support meeting creation payloads.
    - Add a `toEntity` overload to map a `MeetingResponseDto` back to a
      `Meeting` entity.
    - Set `NullValuePropertyMappingStrategy.IGNORE` to prevent null
      properties from overwriting existing entity values, which is required
      by the new `toCreateDto` mapping.

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/meeting/MeetingMapper.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/meeting/MeetingMapper.java
@@ -13,13 +13,16 @@ import java.util.List;
 @Mapper(
     componentModel = "spring",
     uses = {AttendanceMapper.class, StrikeMapper.class, ParticipationPointMapper.class},
-    collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED
+    collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
 )
 public interface MeetingMapper {
+    MeetingCreateDto toCreateDto(Meeting meeting);
+
     MeetingResponseDto toResponseDto(Meeting meeting);
     List<MeetingResponseDto> toResponseDtoList(List<Meeting> meetings);
 
     Meeting toEntity(MeetingCreateDto dto);
-
     Meeting toEntity(MeetingUpdateDto dto);
+    Meeting toEntity(MeetingResponseDto dto);
 }
