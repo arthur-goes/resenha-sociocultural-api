36c4cb4fad412c669199580c29b207886921d81a
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 12:15:32 -0300

    refactor(youth): adapt YouthEntryDto interface to use nested YouthSimpleDto

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/youth/dto/YouthEntryDto.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/youth/dto/YouthEntryDto.java
@@ -1,7 +1,5 @@
 package br.com.resenhasociocultural.apiresenha.features.youth.dto;
 
 public interface YouthEntryDto {
-    Long youthId();
-    String youthFirstName();
-    String youthSurname();
+    YouthSimpleDto youth();
 };
