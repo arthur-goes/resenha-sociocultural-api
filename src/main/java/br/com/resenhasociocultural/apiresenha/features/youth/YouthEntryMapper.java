56db391248a6ed6242a26419ccd0dd26e829ec8d
Arthur Góes <paschoal.ag@gmail.com>
2025-11-23 11:36:20 -0300

    refactor(youth): remove unused @AfterMappingMethod from YouthEntryMapper
    This method is no longer needed. The new creation DTOs for youth entries
    (Attendance, Strike, and ParticipationPoint) now use a nested
    `YouthSimpleDto` instead of plain youth attributes, making the manual
    mapping redundant.

--- src/main/java/br/com/resenhasociocultural/apiresenha/features/youth/YouthEntryMapper.java
+++ src/main/java/br/com/resenhasociocultural/apiresenha/features/youth/YouthEntryMapper.java
@@ -1,22 +1,12 @@
 package br.com.resenhasociocultural.apiresenha.features.youth;
 
 import br.com.resenhasociocultural.apiresenha.features.youth.dto.YouthEntryDto;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Context;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 public interface YouthEntryMapper<Entry extends YouthEntry, EntryDto extends YouthEntryDto> {
 
     Entry toEntity(EntryDto entryDto);
 
-    @AfterMapping
-    default void linkYouth(EntryDto entryDto, @MappingTarget Entry youthEntry){
-        Youth youth = new Youth();
-        youth.setId(entryDto.youthId());
-        youth.setFirstName(entryDto.youthFirstName());
-        youth.setSurname(entryDto.youthSurname());
+    EntryDto toCreateDto(Entry entry);
 
-        youthEntry.setYouth(youth);
-    };
 }
